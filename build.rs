15a5190ace5607ba1f03c54d7e3a9f22ffff58a5
Daniel Szecket <szecket@magrittescow.com>
2026-03-27 19:01:29 +0000

    more lib stuffs

--- build.rs
+++ build.rs
@@ -2,10 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 fn main() {
-    // Expose the UI directory path to dependent crates
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let ui_path = std::path::Path::new(&manifest_dir).join("ui");
-    println!("cargo:SLINT_LIBRARY_PATH={}", ui_path.display());
-    
-    slint_build::compile("ui/radial-menu.slint").unwrap();
+    println!("cargo:slint_library_path={}/ui", manifest_dir);
 }
