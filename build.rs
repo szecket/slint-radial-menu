96e5a010f05d85faf9c2aba73d7fa44bf5b8555f
szecket <szecket@magrittescow.com>
2026-03-27 15:22:49 -0400

    last lib stuffs (#3)

--- build.rs
+++ build.rs
@@ -4,4 +4,6 @@
 fn main() {
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     println!("cargo:slint_library_path={}/ui", manifest_dir);
+    
+    slint_build::compile("ui/radial-menu.slint").unwrap();
 }
